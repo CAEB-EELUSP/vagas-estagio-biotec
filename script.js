aed1515ec1ae7c5edbb2b986c49c629f59d346c6
CAEB-EELUSP <caeb.usp@gmail.com>
2025-09-22 11:54:34 -0300

    script.js

--- script.js
+++ script.js
@@ -1,56 +1,41 @@
-// --- Inicialização do mapa ---
-const sudesteBounds = L.latLngBounds(
-  [
-    [-25.5, -52.0], // canto sudoeste
-    [-17.0, -38.0]  // canto nordeste
-  ]
-);
+// Definir limites do Sudeste
+const sudesteBounds = L.latLngBounds([
+  [-25.5, -52.0], // canto sudoeste
+  [-17.0, -38.0]  // canto nordeste
+]);
 
+// Criar mapa dentro dos limites
 const map = L.map('map', {
   maxBounds: sudesteBounds,
-  maxBoundsViscosity: 1.0,
-  minZoom: 6
+  maxBoundsViscosity: 1.0
 });
 map.fitBounds(sudesteBounds);
+map.setMinZoom(map.getBoundsZoom(sudesteBounds));
 
-// --- Camada base ---
+// Adicionar camada de tiles do OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// --- Ícone personalizado laranja ---
+// Ícone laranja personalizado
 const orangeIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
 });
 
-// --- Marcadores com ícone laranja ---
+// Exemplos de marcadores no Sudeste
 L.marker([-23.55, -46.63], { icon: orangeIcon }).addTo(map)
-  .bindPopup("<b>São Paulo</b><br>Capital do estado de SP");
+  .bindPopup("São Paulo");
 
 L.marker([-22.90, -43.20], { icon: orangeIcon }).addTo(map)
-  .bindPopup("<b>Rio de Janeiro</b><br>Capital do estado do RJ");
+  .bindPopup("Rio de Janeiro");
 
 L.marker([-19.92, -43.94], { icon: orangeIcon }).addTo(map)
-  .bindPopup("<b>Belo Horizonte</b><br>Capital de MG");
+  .bindPopup("Belo Horizonte");
 
-L.marker([-20.32, -40.34], { icon: orangeIcon }).addTo(map)
-  .bindPopup("<b>Vitória</b><br>Capital do ES");
-
-// --- (Opcional) Desenhar os estados do Sudeste ---
-fetch("sudeste.geojson")
-  .then(res => res.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: {
-        color: "blue",
-        weight: 2,
-        fillOpacity: 0.05
-      }
-    }).addTo(map);
-  });
-
+L.marker([-22.90, -47.06], { icon: orangeIcon }).addTo(map)
+  .bindPopup("Campinas");
