ae941a2436d3c4dd138be336a8052069c7d94947
CAEB-EELUSP <caeb.usp@gmail.com>
2025-09-22 11:48:40 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// --- Inicialização do mapa ---// --- Inicialização do mapa ---
+// --- Inicialização do mapa ---
 const sudesteBounds = L.latLngBounds(
   [
     [-25.5, -52.0], // canto sudoeste
@@ -7,33 +7,41 @@ const sudesteBounds = L.latLngBounds(
 );
 
 const map = L.map('map', {
-  maxBounds: sudesteBounds,        // limita o arraste
-  maxBoundsViscosity: 1.0,         // "gruda" nas bordas
-  minZoom: 6                       // impede zoom-out além do Sudeste
+  maxBounds: sudesteBounds,
+  maxBoundsViscosity: 1.0,
+  minZoom: 6
 });
-
-// Ajusta a visualização inicial
 map.fitBounds(sudesteBounds);
 
-// --- Camada base (OpenStreetMap) ---
+// --- Camada base ---
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-// --- Exemplo de marcadores ---
-L.marker([-23.55, -46.63]).addTo(map)
+// --- Ícone personalizado laranja ---
+const orangeIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+// --- Marcadores com ícone laranja ---
+L.marker([-23.55, -46.63], { icon: orangeIcon }).addTo(map)
   .bindPopup("<b>São Paulo</b><br>Capital do estado de SP");
 
-L.marker([-22.90, -43.20]).addTo(map)
+L.marker([-22.90, -43.20], { icon: orangeIcon }).addTo(map)
   .bindPopup("<b>Rio de Janeiro</b><br>Capital do estado do RJ");
 
-L.marker([-19.92, -43.94]).addTo(map)
+L.marker([-19.92, -43.94], { icon: orangeIcon }).addTo(map)
   .bindPopup("<b>Belo Horizonte</b><br>Capital de MG");
 
-L.marker([-20.32, -40.34]).addTo(map)
+L.marker([-20.32, -40.34], { icon: orangeIcon }).addTo(map)
   .bindPopup("<b>Vitória</b><br>Capital do ES");
 
-// --- (Opcional) Carregar o GeoJSON dos estados ---
+// --- (Opcional) Desenhar os estados do Sudeste ---
 fetch("sudeste.geojson")
   .then(res => res.json())
   .then(data => {
@@ -45,5 +53,4 @@ fetch("sudeste.geojson")
       }
     }).addTo(map);
   });
-
 
