8f0f44d9b224be9f35440e763ad64f08fad9c7d9
CAEB-EELUSP <caeb.usp@gmail.com>
2025-12-09 15:11:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -127,13 +127,13 @@ function popupHtml(emp) {
     <div style="min-width:220px">
       <strong>${emp.nome}</strong>
       <div style="margin-top:4px;color:#555">${emp.cidade ?? ""}</div>
-      <div style="style="
+      <div style="
           margin-top:4px;
           color:#777; 
           font-size: 0.85rem; 
           font-family: 'Segoe UI', sans-serif;
     "><i>
-        ${emp.area ?? (Array.isArray(emp.areas) ? emp.areas.join(", ") : "")}</i></div>
+        #${emp.area ?? (Array.isArray(emp.areas) ? emp.areas.join(", ") : "")}</i></div>
       <div style="margin-top:12px;color:#C14904"><strong>${emp.telefone ?? ""}</strong>
       <div style="margin-top:4px;color:#C14904"><strong>${emp.email ?? ""}</strong>
       </div>
@@ -166,5 +166,6 @@ fetch('empresas.json')
   .catch(err => {
     console.error('Erro ao carregar empresas.json', err);
   });
+
 
 
